62cba47c52379a2e5981e775985208e66b6633eb
Mindsurfer7 <bamboocymba@icloud.com>
2023-02-20 01:38:26 +0600

    buttons+

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ function App() {
   return (
     <div className={appCss}>
       <button onClick={onclose}>close</button>
+      <button onClick={onclose}>open</button>
+      <button onClick={onclose}>kek</button>
+      <button onClick={onclose}>x</button>
+      <button onClick={onclose}>y</button>
     </div>
   );
 }
